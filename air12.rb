ceeac585f8f043cdd74c6ca971dc1559ae00061d
Oliver KEB <oliver.keb@icloud.com>
2022-07-26 08:57:27 +0200

    :rotating_light: feature(air): pas encore terminé

---
+++ air12.rb
@@ -0,0 +1,48 @@
+# Créez un programme qui trie une liste de nombres. Votre programme devra implémenter
+# l’algorithme du tri rapide (QuickSort).
+
+
+# LES FONCTIONS
+# -------------
+def is_numeric char
+  char.match(/\A[+-]?\d+?(_?\d+)*(\.\d+e?\d*)?\Z/) != nil
+end
+def my_quick_sort(list)
+  sorted_table = list
+  puts "execution..."
+  sorted_table
+end
+# -------------
+
+
+# LES ERREURS
+# -------------
+if ARGV.length < 1
+  puts 'erreur'
+  exit
+end
+
+(0..(ARGV.length - 1)).each do |i|
+  if !is_numeric(ARGV[i])  && (i != ARGV.index("fusion"))
+    puts 'erreur'
+    exit
+  end
+end
+# -------------
+
+
+# LES DATA
+# -------------
+list = ARGV
+# -------------
+
+# LA RESOLUTION
+# -------------
+result = my_quick_sort(list)
+# -------------
+
+
+# RESULTAT(S)
+# -------------
+puts result.join ' '
+# -------------
